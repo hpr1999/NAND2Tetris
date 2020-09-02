225563a66f91ab6603d90c98926887c6f450ca5f
Hans Rißer <hans.r.1999@gmail.com>
2020-09-02 14:09:55 +0200

    + SymbolAccessInstructions

--- projects/06/Hack-Assembler/src/main/java/instruction/SymbolAccessInstruction.java
+++ projects/06/Hack-Assembler/src/main/java/instruction/SymbolAccessInstruction.java
@@ -1,63 +1,51 @@
 package instruction;
 
 import base.SymbolTable;
-import util.BinaryUtil;
+import util.MnemonicUtil;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class SymbolAccessInstruction extends AddressInstruction {
-
-//    TODO we need to deal with inbuilt Symbols, but also with variables,
-//     that can be created BY an Access Instruction AND we need to deal
-//      with the fact, that we might not supply a table or might not have one
-//      in the super constructor that calls isValid
-
-    private SymbolTable table = null;
-
-    public SymbolAccessInstruction(int integerRepresentation) {
-        this(integerRepresentation, translate(integerRepresentation), null);
-    }
 
     public SymbolAccessInstruction(String stringRepresentation, SymbolTable table) {
         this(translate(stringRepresentation, table), stringRepresentation, table);
     }
 
     protected SymbolAccessInstruction(int integerRepresentation, String stringRepresentation, SymbolTable table) {
+
+        checkNotNull(table);
+        checkNotNull(stringRepresentation);
+
         this.integerRepresentation = integerRepresentation;
         this.stringRepresentation = stringRepresentation;
-        this.table = table;
-        if (table == null) {
-            this.table = new SymbolTable() {
-                @Override
-                public boolean hasSymbol(Symbol symbol) {
-                    return true;
-                }
-            };
-        }
-//        TODO create new Variable Symbols if necessary.
+        Symbol symbol = extractSymbol(stringRepresentation);
+
+        createVariableIfNecessary(table, symbol);
         checkArgument(!(stringRepresentation.isEmpty()) && isValid(),
                 "%s and %s do not form a valid Instruction.",
                 integerRepresentation, stringRepresentation);
     }
 
-    @Override
-    public boolean isValidMnemonic() {
-        return isValidMnemonic(stringRepresentation, table);
+    private static void createVariableIfNecessary(SymbolTable table, Symbol symbol) {
+        if (!table.hasSymbol(symbol)) {
+            table.createVariable(symbol);
+        }
     }
 
-    public static boolean isValidMnemonic(String stringRepresentation, SymbolTable table) {
-        return AddressInstruction.isValidMnemonic(stringRepresentation) ||
-                AddressInstruction.ADDRESS_IDENTIFIER.equals(stringRepresentation.charAt(0)) &&
-                        table.hasSymbol(extractSymbol(stringRepresentation));
+    @Override
+    public boolean isValidMnemonic() {
+        return isValidMnemonic(stringRepresentation);
+    }
+
+    public static boolean isValidMnemonic(String stringRepresentation) {
+        return '@' == stringRepresentation.charAt(0) && MnemonicUtil.hasLettersAndCanHaveDigits(stringRepresentation.substring(1));
     }
 
     public static int translate(String mnemonic, SymbolTable table) {
-//        TODO if necessary, create new Variable Symbol.
-        checkArgument(isValidMnemonic(mnemonic, table), "%s is not a valid mnemonic.", mnemonic);
-        int referencedLine = table.getLine(extractSymbol(mnemonic));
-        String stringOfLineNumber = BinaryUtil.fixedLengthBinaryString(referencedLine,
-                BinaryUtil.BINARY_WORD_LENGTH - 1);
-        return AddressInstruction.translate(AddressInstruction.ADDRESS_IDENTIFIER + stringOfLineNumber);
+        createVariableIfNecessary(table, extractSymbol(mnemonic));
+        checkArgument(isValidMnemonic(mnemonic), "%s is not a valid mnemonic.", mnemonic);
+        return table.getLine(extractSymbol(mnemonic));
     }
 
     private static Symbol extractSymbol(String mnemonic) {
--- projects/06/Hack-Assembler/src/test/java/instruction/SymbolAccessInstructionTest.java
+++ projects/06/Hack-Assembler/src/test/java/instruction/SymbolAccessInstructionTest.java
@@ -1,7 +1,83 @@
 package instruction;
 
+import base.SymbolTable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SymbolAccessInstructionTest {
-// FIXME untested
+
+
+    private SymbolTable table;
+    private SymbolAccessInstruction instruction;
+
+    @BeforeEach
+    void setUp() {
+        table = new SymbolTable();
+    }
+
+    @Test
+    void inbuiltSymbols() {
+        test("@KBD", 24576);
+        test("@SCREEN", 16384);
+        test("@SP", 0);
+        test("@LCL", 1);
+        test("@ARG", 2);
+        test("@THIS", 3);
+        test("@THAT", 4);
+        for (int i = 0; i <= 15; i++) {
+            test("@R" + i, i);
+        }
+    }
+
+    @Test
+    void variables() {
+        test("@v1", 16);
+        test("@v2", 17);
+        test("@v3", 18);
+        test("@v4", 19);
+
+        test("@v1", 16);
+        test("@v2", 17);
+        test("@v3", 18);
+        test("@v4", 19);
+
+        table.createVariable(new Symbol("v5"));
+        test("@v5", 20);
+        test("@v5", 20);
+    }
+
+    @Test
+    void symbols() {
+        table.addSymbol(new Symbol("S101"), 101);
+        table.addSymbol(new Symbol("S102"), 102);
+        table.addSymbol(new Symbol("S103"), 103);
+
+        test("@S101", 101);
+        test("@S102", 102);
+        test("@S103", 103);
+    }
+
+    @Test
+    void mix() {
+        table.createVariable(new Symbol("v5"));
+        table.addSymbol(new Symbol("S15"), 15);
+        table.addSymbol(new Symbol("S16"), 16);
+        test("@S15", 15);
+        test("@S16", 16);
+        test("@R15", 15);
+        test("@v5", 16);
+    }
+
+    @Test
+    void invalid() {
+        assertThrows(IllegalArgumentException.class, () -> test("@123", 123));
+        assertThrows(IllegalArgumentException.class, () -> test("@Ä123", 123));
+    }
+
+    private void test(String mnemonic, int machineCode) {
+        instruction = new SymbolAccessInstruction(mnemonic, table);
+        assertEquals(machineCode, instruction.machineCode());
+    }
 }
