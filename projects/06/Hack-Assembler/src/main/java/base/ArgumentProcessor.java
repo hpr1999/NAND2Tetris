36c09ddb2a3f2f6a0198740fd72f7c6c76e36e36
Hans Rißer <hans.r.1999@gmail.com>
2020-09-02 15:13:50 +0200

    + add output file argument

--- projects/06/Hack-Assembler/src/main/java/base/ArgumentProcessor.java
+++ projects/06/Hack-Assembler/src/main/java/base/ArgumentProcessor.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class ArgumentProcessor {
 
     private static final String asmFileKey = "asm-file-path";
+    private static final String hackFileKey = "hack-file-path";
     private String[] arguments;
     private Map<String, String> argumentValues;
 
@@ -19,6 +20,11 @@ public class ArgumentProcessor {
         argumentValues = new HashMap<>();
         if (arguments.length > 0 && arguments[0] != null) {
             argumentValues.put(asmFileKey, arguments[0]);
+
+            if (arguments.length > 1 && arguments[1] != null)
+                argumentValues.put(hackFileKey, arguments[1]);
+            else argumentValues.put(hackFileKey, hackFileFromAsmFile());
+
             return true;
         } else return false;
     }
@@ -28,5 +34,19 @@ public class ArgumentProcessor {
             return Paths.get(argumentValues.get(asmFileKey));
 
         return null;
+    }
+
+    public Path getHackFilePath() {
+        if (argumentValues != null || this.process())
+            return Paths.get(argumentValues.get(hackFileKey));
+
+        return null;
+    }
+
+    private String hackFileFromAsmFile() {
+        String asm = argumentValues.get(asmFileKey);
+        int index = asm.lastIndexOf('.');
+        index = index != -1 ? index : asm.length() - 1;
+        return asm.substring(0, index) + ".hack";
     }
 }
