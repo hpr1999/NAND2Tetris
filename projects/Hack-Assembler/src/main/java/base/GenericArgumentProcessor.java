df4c2813bd93f2ad8e7336c4f53d205925915b30
Hans Rißer <hans.r.1999@gmail.com>
2020-09-09 19:17:32 +0200

    + alternative argument processing

---
+++ projects/Hack-Assembler/src/main/java/base/GenericArgumentProcessor.java
@@ -0,0 +1,50 @@
+package base;
+
+import java.util.*;
+import java.util.function.Function;
+
+import static com.google.common.base.Preconditions.*;
+
+public class GenericArgumentProcessor {
+
+    private final Map<Argument, Object> valuesPerArgument = new HashMap<>();
+    private final Queue<Argument<?>> arguments = new LinkedList<>();
+    private final Queue<String> incomingArguments;
+
+    public GenericArgumentProcessor(String[] argumentValues) {
+        this.incomingArguments = new LinkedList<>(Arrays.asList(argumentValues));
+    }
+
+    public void registerArgument(Argument<?> argument) {
+        checkArgument(!arguments.contains(argument), "This argument was already added.");
+        arguments.add(argument);
+    }
+
+    public void process() {
+        while (!arguments.isEmpty() && !incomingArguments.isEmpty()) {
+            Argument argument = arguments.poll();
+            Object value = argument.transformer.apply(incomingArguments.poll());
+            valuesPerArgument.put(argument, value);
+        }
+    }
+
+    public <T> T get(Argument<T> argument) {
+        if (valuesPerArgument.containsKey(argument))
+            return (T) valuesPerArgument.get(argument);
+        else {
+            checkState(argument.defaultSupplier != null, "This argument was not processed and has no default.");
+            return argument.defaultSupplier.apply(this);
+        }
+    }
+
+    public static class Argument<T> {
+        private final Function<String, T> transformer;
+        private final Function<GenericArgumentProcessor, T> defaultSupplier;
+
+        public Argument(Function<String, T> transformer, Function<GenericArgumentProcessor, T> defaultSupplier) {
+            checkNotNull(transformer);
+            this.transformer = transformer;
+            this.defaultSupplier = defaultSupplier;
+        }
+    }
+}
---
+++ projects/Hack-Assembler/src/main/java/base/Nand2TetrisArgumentProcessor.java
@@ -0,0 +1,38 @@
+package base;
+
+import hack.parsing.Assembler;
+import hack.parsing.Disassembler;
+import hack.parsing.SmartDisassembler;
+import hack.parsing.Translator;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.BiFunction;
+
+public class Nand2TetrisArgumentProcessor extends GenericArgumentProcessor {
+
+    public static final Argument<BiFunction<Path, Path, Translator>> OPERATION = new Argument<>(
+            argument -> {
+                switch (argument) {
+                    case "assemble":
+                        return Assembler::new;
+                    case "disassemble":
+                        return Disassembler::new;
+                    case "smart-disassemble":
+                        return SmartDisassembler::new;
+                    default:
+                        return null;
+                }
+            }, null);
+    public static final Argument<Path> INPUT_FILE = new Argument<>(Paths::get, null);
+    public static final Argument<Path> OUTPUT_FILE = new Argument<>(Paths::get, null);
+
+    public Nand2TetrisArgumentProcessor(String[] incomingArguments) {
+        super(incomingArguments);
+        registerArgument(OPERATION);
+        registerArgument(INPUT_FILE);
+        registerArgument(OUTPUT_FILE);
+        process();
+    }
+
+}
